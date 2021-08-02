29bd675f530bf28fcd51a58f2dd4d0f75af78b23
Gowtham Namuri <gowtham.namuri@ymedialabs.com>
2021-08-02 08:07:37 +0530

    Move 'FeedCachePolicy' to its own file.

---
+++ EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,26 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialFeed
+//
+//  Created by Gowtham Namuri on 02/08/21.
+//
+
+import Foundation
+
+internal final class FeedCachePolicy {
+    
+    private init() {}
+    
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    private static var maxCacheAgeInDays: Int {
+        return 7
+    }
+    
+    static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
+            return false
+        }
+        return date < maxCacheAge
+    }
+}
